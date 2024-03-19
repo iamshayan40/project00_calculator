e446fc010aa7910f230f557078b9313b52cb4e7c
Shayan <shayanwaseem.50@gmail.com>
2024-03-20 00:08:22 +0500

    push my calculator to github

---
+++ index.js
@@ -0,0 +1,30 @@
+// 1ST OF ALL WE WILL OPEN CMD AND WRITE npm init THEN THE PACKAGE WILL ASK SOME DETAILS ABOUT OUR APPLICATION.
+// #HERE WE IMPORTED INQUIRER FROM INQUIRER PACKAGE WHICH WE HAVE INSTALLED BY CMD. 
+import inquirer from "inquirer";
+// WE PUT A CONST DATA TYPE COZ VALUE WILL BE CONSTANT THEN MADE A ANSWER VARIABLE THEN WE SAID AWAIT OF INQUIRE.PRINT WHICH USER WILL ENTER.
+const answer = await inquirer.prompt([
+    { message: "enter first number", type: "number", name: "firstnumber" },
+    { message: "enter second number", type: "number", name: "secondnumber" },
+    {
+        message: "select one of the operators to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    }
+]);
+//Conditional Statments
+if (answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("Please select valid operator");
+}
