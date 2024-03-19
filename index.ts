4d39352ace02ee5c8b9d27a6554f6dc99e96c146
Shayan <shayanwaseem.50@gmail.com>
2024-03-20 00:17:57 +0500

    I've done some minor changes

--- index.ts
+++ index.ts
@@ -4,8 +4,8 @@ import inquirer from "inquirer";
 
 // WE PUT A CONST DATA TYPE COZ VALUE WILL BE CONSTANT THEN MADE A ANSWER VARIABLE THEN WE SAID AWAIT OF INQUIRE.PRINT WHICH USER WILL ENTER.
 const answer = await inquirer.prompt([
-    {message: "enter first number" ,type: "number" ,name: "firstnumber"},
-    {message: "enter second number" ,type: "number" ,name: "secondnumber"},
+    {message: "enter your first number" ,type: "number" ,name: "firstnumber"},
+    {message: "enter your second number" ,type: "number" ,name: "secondnumber"},
     {
         message: "select one of the operators to perform action",
         type: "list",
